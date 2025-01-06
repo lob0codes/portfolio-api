ed7ee83846d86660ad1a9ed3f23744418e97574d
lob0codes <lob0codes@gmail.com>
2025-01-06 12:08:37 -0600

    Configured settings.py file to target local environment config.

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 
-import os
 from pathlib import Path
 import environ
 env = environ.Env()
@@ -24,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('CLOUD_SECRET_KEY')
+SECRET_KEY = env('LOCAL_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['lob0codes.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -83,11 +82,12 @@ WSGI_APPLICATION = 'api_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('PA_DB_NAME'),
-        'USER': env('PA_DB_USER'),
-        'PASSWORD': env('PA_DB_PASSWORD'),
-        'HOST': env('PA_DB_HOST')
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT')
     }
 }
 
@@ -136,17 +136,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',  # The default port for create-react-app
 ]
-
-REST_FRAMEWORK = {
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',  # For anonymous users
-        'rest_framework.throttling.UserRateThrottle',  # For authenticated users
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',  # Limit anonymous users to 100 requests per day
-        'user': '1000/day',  # Limit authenticated users to 1000 requests per day
-    }
-}
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
